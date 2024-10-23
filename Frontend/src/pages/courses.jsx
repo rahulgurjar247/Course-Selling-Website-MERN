12ddfe82d87a0f09450f873e506ede3290d1bbb5
rahul <rahulgurjar824582@gmail.com>
2024-10-23 22:00:00 +0530

    update course card

--- Frontend/src/pages/courses.jsx
+++ Frontend/src/pages/courses.jsx
@@ -1,31 +1,12 @@
 import { toast } from "react-toastify";
-import devloper from "../assets/developer.png";
 import { useEffect, useState } from "react";
-
-export function CourseCard( id="n",title="hello",description="nothing",price="500") {
-  return (
-    <>
-     <div className="bg-white  w-72 h- rounded-3xl overflow-hidden">
-              <img src={devloper} className="bg-slate-400 w-full h-48" />
-              <div className="p-4 pt-2 ">
-                <div className="text-center text-wrap text-xl">{title}</div>
-                <div className="text-pretty">
-                  {description}
-                </div>
-                <div className=" text-wrap text-xl">
-                  price:<span className="text-blue-700">{price}</span>
-                </div>
-              </div>
-            </div>
-    </>
-  )
-}  
+import CourseCard from "../Component/courseCard";
 
 function Courses() {
   const [courses, setcourses] = useState([]);
-  
+
   useEffect(() => {
-    (async function fetchdata(){
+    (async function fetchdata() {
       try {
         const res = await fetch("http://localhost:8000/users/courses", {
           method: "GET",
@@ -34,7 +15,7 @@ function Courses() {
           },
           credentials: "include",
         });
-  
+
         const data = await res.json();
         const result = await data.course;
         setcourses(result);
@@ -42,31 +23,21 @@ function Courses() {
       } catch (err) {
         toast.error(err);
       }
-    }());    
-  },[])
+    })();
+  }, []);
 
   return (
     <>
-      <div
-        className="min-h-[calc(100vh-60px)]  bg-slate-100 w-full p-20  pb-10 flex gap-14 flex-wrap font-mono"
-      >
+      <div className="min-h-[calc(100vh-60px)]  bg-slate-100 w-full p-20  pb-10 flex gap-14 flex-wrap font-mono">
         {courses.map((course) => {
           return (
-            <div
-              className="bg-white  w-72 h- rounded-3xl overflow-hidden"
-              key={course.title}
-            >
-              <img src={devloper} className="bg-slate-400 w-full h-48" />
-              <div className="p-4 pt-2 ">
-                <div className="text-center text-wrap text-xl">
-                  {course.title}
-                </div>
-                <div className="text-pretty">{course.description}</div>
-                <div className=" text-wrap text-xl">
-                  price:<span className="text-blue-700">{course.price}</span>
-                </div>
-              </div>
-            </div>
+            <CourseCard
+              id={course._id}
+              title={course.title}
+              description={course.description}
+              price={course.price}
+              key={course._id}
+            />
           );
         })}
       </div>
